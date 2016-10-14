2f50b6c3f2aaa76851afe33105ffe855d6b08839
Kenneth Orton <korton@asu.edu>
2016-10-14 08:04:28 -0700

    Update create_LDA_model.py

--- create_LDA_model.py
+++ create_LDA_model.py
@@ -9,21 +9,11 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 
 NUM_TOPICS = 75
 
-### Working with persisted corpus and dictionary
 bow_corpus = MmCorpus('data/wiki_corpus.mm') 
 dictionary = Dictionary.load('data/wiki_dict.dict')
 
-#lda = gensim.models.LdaModel(corpus=bow_corpus, id2word=dictionary, num_topics=100, update_every=1, chunksize=10000, passes=1)
-#lda = gensim.models.LdaMulticore(corpus=bow_corpus, id2word=dictionary, num_topics=NUM_TOPICS, workers=7)
 lda = gensim.models.LdaMulticore(corpus=bow_corpus, id2word=dictionary, num_topics=NUM_TOPICS, workers=7, passes=5)
 lda.save('data/lda_75_lem_5_pass.model')
 
 vis_data = gensimvis.prepare(lda, bow_corpus, dictionary)
 pyLDAvis.save_html(vis_data, 'data/lda_75_lem_5_pass.html')
-'''
-lda = gensim.models.LdaMulticore(corpus=bow_corpus, id2word=dictionary, num_topics=75, workers=7)
-lda.save('data/lda_75_lem.model')
-
-vis_data = gensimvis.prepare(lda, bow_corpus, dictionary)
-pyLDAvis.save_html(vis_data, 'data/lda_75_lem.html')
-'''
