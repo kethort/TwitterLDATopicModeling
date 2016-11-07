eb076f9725ddb819edbf9e4b4f73004a3c490d95
Kenneth Orton <korton@asu.edu>
2016-11-07 02:12:23 -0700

    added multiprocessing

--- tweet_corpus.py
+++ tweet_corpus.py
@@ -1,10 +1,14 @@
 # Source for Gensim library and tutorials: http://radimrehurek.com/gensim/index.html
+# http://stackoverflow.com/questions/26568722/remove-unicode-emoji-using-re-in-python
+# http://stackoverflow.com/questions/11331982/how-to-remove-any-url-within-a-string-in-python
+# https://pymotw.com/2/multiprocessing/mapreduce.html
 import logging
 import os
 import sys
 import re
 import itertools
 import tarfile
+import multiprocessing
 import gensim
 from gensim.corpora import MmCorpus, Dictionary
 
@@ -15,41 +19,40 @@ ignore_words = set(stopwords.words('english'))
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
+def list_to_gen(tweets):
+    for filename in tweets:
+        yield 'tweets_dir/' + str(filename)
+
 def preprocess_tweet(tweet):
-    """
-    Preprocess a single tweet
-    """
-    # transform tweet document into one string
-    text = ' '.join(line.rstrip('\n') for line in tweet)
-    # http://stackoverflow.com/questions/26568722/remove-unicode-emoji-using-re-in-python
-    # remove emoji's and links from tweets
-    try:
-        reg_ex = re.compile(u'([\U0001F300-\U0001F64F])|([\U0001F680-\U0001F6FF])|([\U00002600-\U000027BF])')
-    except:
-        reg_ex = re.compile(u'([\u2600-\u27BF])|([\uD83C][\uDF00-\uDFFF])|([\uD83D][\uDC00-\uDE4F])|([\uD83D][\uDE80-\uDEFF])')
-    text = reg_ex.sub('', text)
-    # http://stackoverflow.com/questions/11331982/how-to-remove-any-url-within-a-string-in-python
-    text = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', text)
-    text = re.sub(r'[^\w]', ' ', text) # remove hashtag
-    text = text.replace("'", "")
-    # remove stopwords
-    words = [word for word in text.lower().split() if word not in ignore_words and len(word) > 2]
-    #return list(utils.simple_preprocess(text, deacc=True, min_len=2, max_len=15))
-    text = ' '.join(words)
-    #return text
-    return list(gensim.utils.lemmatize(text))
+    with open(tweet, 'r') as infile:
+        # transform tweet document into one string
+        text = ' '.join(line.rstrip('\n') for line in infile)
+        # remove emoji's
+        try:
+            reg_ex = re.compile(u'([\U0001F300-\U0001F64F])|([\U0001F680-\U0001F6FF])|([\U00002600-\U000027BF])')
+        except:
+            reg_ex = re.compile(u'([\u2600-\u27BF])|([\uD83C][\uDF00-\uDFFF])|([\uD83D][\uDC00-\uDE4F])|([\uD83D][\uDE80-\uDEFF])')
+        # remove URLS
+        text = reg_ex.sub('', text)
+        text = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', text)
+        # remove hashtag symbol
+        text = re.sub(r'[^\w]', ' ', text) 
+        text = text.replace("'", "")
+        # remove stopwords and lemmatize
+        return list(gensim.utils.lemmatize(text, allowed_tags=re.compile('(NN)'), stopwords=ignore_words, min_length=3)
 
 class Text_Corpus(gensim.corpora.TextCorpus):
     def get_texts(self):
-        for filename in self.input:
-            with open('working_folder/tweets_dir/' + filename, 'r') as infile:
-                yield preprocess_tweet(infile)
+        pool = multiprocessing.Pool(4)
+        for tokens in pool.imap(preprocess_tweet, self.input):
+            yield tokens
+        pool.terminate()
 
-tweets = os.listdir('working_folder/tweets_dir/')
+tweets = list_to_gen(os.listdir('tweets_dir/'))
 tweet_corpus = Text_Corpus(tweets)
+# ignore words that appear in less than 5 documents or more than 5% of documents
 tweet_corpus.dictionary.filter_extremes(no_below=5, no_above=0.5, keep_n=DEFAULT_DICT_SIZE)
 
-MmCorpus.serialize('data/author_topic_2.mm', author_topic_corpus, progress_cnt=2000)
-tweet_corpus.dictionary.save('data/author_topic_2.dict')
-
+MmCorpus.serialize('data/sample_tweet_corpus.mm', tweet_corpus, progress_cnt=2000)
+tweet_corpus.dictionary.save('data/sample_tweet_dict.dict')
 
