4d6756ba77189ec049e9e4eb9a4f383d998ec968
Kenneth Orton <korton@asu.edu>
2016-11-14 22:49:56 -0700

    added topn_words and better cmd line param input

--- tweets_on_LDA.py
+++ tweets_on_LDA.py
@@ -12,6 +12,19 @@ import ast
 import multiprocessing
 from functools import partial
 
+# http://stackoverflow.com/questions/15365046/python-removing-pos-tags-from-a-txt-file
+def write_topn_words(user_topics_dir, lda_model):
+    print('Writing topn words for LDA model')
+    reg_ex = re.compile('(?<![\s/])/[^\s/]+(?![\S/])')
+
+    with open(user_topics_dir + 'topn_words.txt', 'w') as outfile:
+        for i in range(lda_model.num_topics):
+            outfile.write('{}\n'.format('Topic #' + str(i + 1) + ': '))
+            for word, prob in lda_model.show_topic(i, topn=20):
+                word = reg_ex.sub('', word)
+                outfile.write('\t{}\n'.format(word.encode('utf-8')))
+            outfile.write('\n')	
+
 def combine_vector_dictionaries(user_topics_dir, community_doc_vecs):
     if os.path.exists(user_topics_dir + 'all_community_doc_vecs.pickle'):
         with open(user_topics_dir + 'all_community_doc_vecs.pickle', 'rb') as all_community_file:
@@ -79,7 +92,7 @@ def convert_to_doc(tweetpath):
     pool.close()
     return document
 
-def get_user_document_vectors(tweetpath, user_id, community_dir, user_topics_dir, community_doc_vecs):
+def get_user_document_vectors(tweetpath, user_id, community_dir, user_topics_dir, community_doc_vecs, dictionary, lda_model):
     all_community_doc_vecs = {}
     if os.path.exists(user_topics_dir + 'all_community_doc_vecs.pickle'):
         with open(user_topics_dir + 'all_community_doc_vecs.pickle', 'rb') as all_community_file:
@@ -88,14 +101,8 @@ def get_user_document_vectors(tweetpath, user_id, community_dir, user_topics_dir
     if not user_id in all_community_doc_vecs:
         document = convert_to_doc(tweetpath)
 
-        # load wiki dictionary
-        dictionary = corpora.Dictionary.load('data/tweets.dict')
-
         # create bag of words from input document
         doc_bow = dictionary.doc2bow(document)
-
-        # load trained wiki model from file
-        lda_model = models.LdaModel.load('data/tweets_100_lem_5_pass.model')
 
         # queries the document against the LDA model and associates the data with probabalistic topics
         doc_lda = get_doc_topics(lda_model, doc_bow)
@@ -111,13 +118,20 @@ def get_doc_topics(lda, bow):
     gamma, _ = lda.inference([bow])
     topic_dist = gamma[0] / sum(gamma[0])
     return [(topic_id, topic_value) for topic_id, topic_value in enumerate(topic_dist)]
-        
-def main(arg1):
-    user_topics_dir = 'user_topics_ex/'
+
+# arg1: topology file, arg2: name of directory to create, arg3: dictionary, arg4: lda model         
+def main(arg1, arg2, arg3, arg4):
+    user_topics_dir = arg2 + '/'
 
     # create output directories
     if not os.path.exists(os.path.dirname(user_topics_dir)):
         os.makedirs(os.path.dirname(user_topics_dir), 0o755)
+
+    # load wiki dictionary
+    dictionary = corpora.Dictionary.load(arg3)
+
+    # load trained wiki model from file
+    lda_model = models.LdaModel.load(arg4)
 
     with open(arg1, 'r') as topology_file:
         for i, community in enumerate(topology_file):
@@ -142,18 +156,20 @@ def main(arg1):
                         continue
                     if tmp_doc_vecs:
                         if user_id not in tmp_doc_vecs:
-                            get_user_document_vectors(tweetpath, user_id, community_dir, user_topics_dir, community_doc_vecs)
+                            get_user_document_vectors(tweetpath, user_id, community_dir, user_topics_dir, community_doc_vecs, dictionary, lda_model)
                         else:
                             community_doc_vecs[user_id] = tmp_doc_vecs[user_id]
                             continue
                     else:
-                        get_user_document_vectors(tweetpath, user_id, community_dir, user_topics_dir, community_doc_vecs)
+                        get_user_document_vectors(tweetpath, user_id, community_dir, user_topics_dir, community_doc_vecs, dictionary, lda_model)
  
             combine_vector_dictionaries(user_topics_dir, community_doc_vecs)
  
             # save each community document vector dictionary for later use
             with open(community_dir + '/community_doc_vecs.pickle', 'wb') as community_doc_vecs_file:
                 pickle.dump(community_doc_vecs, community_doc_vecs_file, -1)
+
+	write_topn_words(user_topics_dir, lda_model)
             
 if __name__ == '__main__':
-    sys.exit(main(sys.argv[1]))
+    sys.exit(main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]))
