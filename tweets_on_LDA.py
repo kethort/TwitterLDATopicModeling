cc5bd426fed0721c5aee8d1d470c39b2cf98fb14
Kenneth Orton <korton@asu.edu>
2016-11-29 18:29:46 -0700

    implemented faster multiprocessing

--- tweets_on_LDA.py
+++ tweets_on_LDA.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-import click
-import csv
 import numpy as np
 import gensim
 from gensim import utils, corpora, models
-import pickle
+import json
 import sys
 import re
 import os
@@ -14,56 +12,32 @@ from functools import partial
 
 # http://stackoverflow.com/questions/15365046/python-removing-pos-tags-from-a-txt-file
 def write_topn_words(user_topics_dir, lda_model):
-    print('Writing topn words for LDA model')
-    reg_ex = re.compile('(?<![\s/])/[^\s/]+(?![\S/])')
+    if not os.path.exists(user_topics_dir + 'topn_words.txt'):
+        print('Writing topn words for LDA model')
+        reg_ex = re.compile('(?<![\s/])/[^\s/]+(?![\S/])')
 
-    with open(user_topics_dir + 'topn_words.txt', 'w') as outfile:
-        for i in range(lda_model.num_topics):
-            outfile.write('{}\n'.format('Topic #' + str(i + 1) + ': '))
-            for word, prob in lda_model.show_topic(i, topn=20):
-                word = reg_ex.sub('', word)
-                outfile.write('\t{}\n'.format(word.encode('utf-8')))
-            outfile.write('\n')	
+        with open(user_topics_dir + 'topn_words.txt', 'w') as outfile:
+            for i in range(lda_model.num_topics):
+                outfile.write('{}\n'.format('Topic #' + str(i + 1) + ': '))
+                for word, prob in lda_model.show_topic(i, topn=20):
+                    word = reg_ex.sub('', word)
+                    outfile.write('\t{}\n'.format(word.encode('utf-8')))
+                outfile.write('\n')	
 
 def combine_vector_dictionaries(user_topics_dir, community_doc_vecs):
-    if os.path.exists(user_topics_dir + 'all_community_doc_vecs.pickle'):
-        with open(user_topics_dir + 'all_community_doc_vecs.pickle', 'rb') as all_community_file:
-            all_community_doc_vecs = pickle.load(all_community_file)
-    else:
+    try:
+        with open(user_topics_dir + 'all_community_doc_vecs.json', 'r') as all_community_file:
+            all_community_doc_vecs = json.load(all_community_file)
+    except:
         all_community_doc_vecs = {}
+
     all_community_doc_vecs.update(community_doc_vecs)
-    with open(user_topics_dir + 'all_community_doc_vecs.pickle', 'wb') as all_community_doc_vecs_file:
-        pickle.dump(all_community_doc_vecs, all_community_doc_vecs_file, -1)
+    with open(user_topics_dir + 'all_community_doc_vecs.json', 'w') as all_community_doc_vecs_file:
+        json.dump(all_community_doc_vecs, all_community_doc_vecs_file, sort_keys=True, indent=4)
 
-# http://www.blopig.com/blog/2016/08/processing-large-files-using-python/
-def process_wrapper(tweetpath, chunk_start, chunk_size):
-    with open(tweetpath) as f:
-        output = []
-        f.seek(chunk_start)
-        lines = f.read(chunk_size).splitlines()
-        
-        for line in lines:
-            output.append(preprocess_text(line))
-
-        return [item for sublist in output for item in sublist]
-
-def chunkify(fname, size=1024*1024):
-    file_end = os.path.getsize(fname)
-    
-    with open(fname, 'r') as f:
-        chunk_end = f.tell()
-    
-        while True:
-            chunk_start = chunk_end
-            f.seek(size, 1)
-            f.readline()
-            chunk_end = f.tell()
-            yield chunk_start, (chunk_end - chunk_start)
-            
-            if chunk_end > file_end:
-                break
-
-def preprocess_text(text):
+def preprocess_text(tweetpath):
+    with open(tweetpath, 'r') as infile:
+        text = ' '.join(line.rstrip('\n') for line in infile)
     # remove emoji's and links from tweets
     # http://stackoverflow.com/questions/26568722/remove-unicode-emoji-using-re-in-python
     try:
@@ -92,14 +66,27 @@ def convert_to_doc(tweetpath):
     pool.close()
     return document
 
-def get_user_document_vectors(tweetpath, user_id, community_dir, user_topics_dir, community_doc_vecs, dictionary, lda_model):
-    all_community_doc_vecs = {}
-    if os.path.exists(user_topics_dir + 'all_community_doc_vecs.pickle'):
-        with open(user_topics_dir + 'all_community_doc_vecs.pickle', 'rb') as all_community_file:
-            all_community_doc_vecs = pickle.load(all_community_file)
+def get_user_document_vectors(inputs, user_id):
+    community_dir = inputs[0]
+    tweets_dir = inputs[1]
+    user_topics_dir = inputs[2]
+    dictionary = inputs[3]
+    lda_model = inputs[4]
+
+    user_id = str(user_id).strip()
+    if os.path.exists(tweets_dir + user_id):
+        tweetpath = tweets_dir + user_id
+    else:
+        return
+
+    try:
+        with open(user_topics_dir + 'all_community_doc_vecs.json', 'r') as all_community_file:
+            all_community_doc_vecs = json.load(all_community_file)
+    except:
+        all_community_doc_vecs = {}
 
     if not user_id in all_community_doc_vecs:
-        document = convert_to_doc(tweetpath)
+        document = preprocess_text(tweetpath)
 
         # create bag of words from input document
         doc_bow = dictionary.doc2bow(document)
@@ -109,9 +96,9 @@ def get_user_document_vectors(tweetpath, user_id, community_dir, user_topics_dir
         dense_vec = gensim.matutils.sparse2full(doc_lda, lda_model.num_topics)
     
         # build dictionary of user document vectors <k, v>(user_id, vec)
-        community_doc_vecs[user_id] = dense_vec
+        return (user_id, dense_vec.tolist())
     else:
-        community_doc_vecs[user_id] = all_community_doc_vecs[user_id]
+        return (user_id, all_community_doc_vecs[user_id])
     
 # http://stackoverflow.com/questions/17310933/document-topical-distribution-in-gensim-lda
 def get_doc_topics(lda, bow):
@@ -119,10 +106,14 @@ def get_doc_topics(lda, bow):
     topic_dist = gamma[0] / sum(gamma[0])
     return [(topic_id, topic_value) for topic_id, topic_value in enumerate(topic_dist)]
 
+def users_to_iter(community):
+    for user in ast.literal_eval(community):
+        yield user
+
 # topology: topology file, output_dir: name of directory to create, dict_loc: dictionary, lda_loc: lda model,
 # dir_prefix: prefix for subdirectories (ie community_1)
 
-# python2.7 tweets_on_LDA.py communities dnld_tweets/ user_topics_ex data/twitter/tweets.dict data/twitter/tweets_100_lem_5_pass.model community
+# python2.7 tweets_on_LDA.py communities user_topics_ex data/twitter/tweets.dict data/twitter/tweets_100_lem_5_pass.model community
 def main(topology, tweets_dir, output_dir, dict_loc, lda_loc, dir_prefix):
     user_topics_dir = output_dir + '/'
 
@@ -143,34 +134,21 @@ def main(topology, tweets_dir, output_dir, dict_loc, lda_loc, dir_prefix):
             if not os.path.exists(os.path.dirname(community_dir)):
                 os.makedirs(os.path.dirname(community_dir), 0o755)
              
+            print('Getting document vectors for community: ' + str(i))
+
             community_doc_vecs = {}
-            tmp_doc_vecs = []
-            if os.path.exists(community_dir + 'community_doc_vecs.pickle'):
-                with open(community_dir + 'community_doc_vecs.pickle', 'rb') as tmp_doc_vecs_file:
-                    tmp_doc_vecs = pickle.load(tmp_doc_vecs_file)
- 
-            progress_label = 'Getting document vectors for community: ' + str(i)
-            with click.progressbar(ast.literal_eval(community), label=progress_label) as bar:
-                for user in bar:
-                    user_id = str(user).strip()
-                    if os.path.exists(tweets_dir + user_id):
-                        tweetpath = tweets_dir + user_id
-                    else:
-                        continue
-                    if tmp_doc_vecs:
-                        if user_id not in tmp_doc_vecs:
-                            get_user_document_vectors(tweetpath, user_id, community_dir, user_topics_dir, community_doc_vecs, dictionary, lda_model)
-                        else:
-                            community_doc_vecs[user_id] = tmp_doc_vecs[user_id]
-                            continue
-                    else:
-                        get_user_document_vectors(tweetpath, user_id, community_dir, user_topics_dir, community_doc_vecs, dictionary, lda_model)
+            pool = multiprocessing.Pool(max(1, multiprocessing.cpu_count() - 1))
+            func = partial(get_user_document_vectors, (community_dir, tweets_dir, user_topics_dir, dictionary, lda_model))
+            doc_vecs = pool.imap(func, users_to_iter(community))
+            doc_vecs = [item for item in doc_vecs if item is not None]
+            pool.terminate()
+            community_doc_vecs = dict(doc_vecs)
  
             combine_vector_dictionaries(user_topics_dir, community_doc_vecs)
  
             # save each community document vector dictionary for later use
-            with open(community_dir + '/community_doc_vecs.pickle', 'wb') as community_doc_vecs_file:
-                pickle.dump(community_doc_vecs, community_doc_vecs_file, -1)
+            with open(community_dir + '/community_doc_vecs.json', 'w') as community_doc_vecs_file:
+                json.dump(community_doc_vecs, community_doc_vecs_file, sort_keys=True, indent=4)
 
 	write_topn_words(user_topics_dir, lda_model)
             
