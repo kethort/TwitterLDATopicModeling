bb32b08913cc8dfbccabf5a9763d1efed6dc3ad7
Kenneth Orton <korton@asu.edu>
2016-12-01 02:17:21 -0700

    Update community_topic_prob_dist.py

--- community_topic_prob_dist.py
+++ community_topic_prob_dist.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import gensim
 from gensim import utils, corpora, models
+import csv
 import ast
 import re
 import os
@@ -36,9 +37,8 @@ def draw_dist_graph(inputs, clique_name):
         for topic_id, dist in enumerate(doc_vecs[clique_name]):
             x_axis.append(topic_id + 1)
             y_axis.append(dist)
-        width = 1 
 
-        plt.bar(x_axis, y_axis, width, align='center', color='r')
+        plt.bar(x_axis, y_axis, width=1, align='center', color='r')
         plt.xlabel('Topics')
         plt.ylabel('Probability')
         plt.title('Topic Distribution for clique')
@@ -49,11 +49,41 @@ def draw_dist_graph(inputs, clique_name):
         plt.savefig(output_dir + clique_name)
         plt.close()
 
+def draw_user_to_clique_graphs(distance_dir, dist_file):
+    x_axis = []
+    y_axis = []
+    fieldnames = ['user', 'clique', 'distance']
+
+    if not os.path.exists(distance_dir + dist_file + '.png'):
+        print('Drawing users to clique for: ' + dist_file)
+        with open(distance_dir + dist_file, 'r') as infile:
+            csv_reader = csv.DictReader(infile, delimiter='\t', fieldnames=fieldnames)
+            for row in csv_reader:
+                x_axis.append(str(row['user']))
+                y_axis.append(float(row['distance']))
+
+        plt.bar(np.arange(1, len(x_axis) + 1, 1), y_axis, width=1, align='center', color='r')
+        #plt.plot(np.arange(1, len(x_axis) + 1, 1), y_axis, 'o', color='r')
+        plt.xlabel('Community Users')
+        plt.ylabel('Divergence from Clique')
+        plt.title('Users to Clique ' + dist_file, fontsize=14, fontweight='bold')
+        plt.xticks(np.arange(1, len(x_axis) + 1, 1), x_axis, rotation='vertical', fontsize=7)
+        plt.subplots_adjust(bottom=0.2)
+        plt.ylim([0, np.log(2) + .01])
+        plt.xlim([0, len(x_axis) + 1])
+        plt.savefig(distance_dir + dist_file)
+        plt.close()
+
 def cliques_to_iter(tweet_folder):
     for path, dirs, files in os.walk(tweet_folder):
         for filename in files:   
             yield filename
         break # stop before traversing into newly created dirs
+
+def distance_files_to_iter(distance_dir):
+    for dist_file in os.listdir(distance_dir):
+        if not dist_file.endswith('.png'):
+            yield dist_file
 
 # clique_top: clique topology, comm_top: community topology, tweets_dir: path of downloaded tweets dir
 # dict_loc: dictionary, lda_loc: lda model,
@@ -94,7 +124,6 @@ def main(clique_top, comm_top, tweets_dir, dict_loc, lda_loc, user_topics_dir):
     func = partial(tlda.get_document_vectors, (output_dir, clique_vecs, dictionary, lda)) 
     doc_vecs = pool.map(func, cliques_to_iter(output_dir))
     doc_vecs = [item for item in doc_vecs if item is not None]
-
     clique_vecs.update(dict(doc_vecs))
 
     with open(output_dir + user_topics_dir + 'document_vectors.json', 'w') as outfile:
@@ -117,10 +146,15 @@ def main(clique_top, comm_top, tweets_dir, dict_loc, lda_loc, user_topics_dir):
             doc_vecs = dict(doc_vecs)
 
             print('Writing Jensen Shannon divergences for community ' + str(i))
-            with open(output_dir + user_topics_dir + 'community_user_distances/jensen_shannon_community_' + str(i), 'w') as outfile:
+            with open(output_dir + user_topics_dir + 'community_user_distances/community_' + str(i), 'w') as outfile:
                 for user in doc_vecs:
                     jsd = pltd.jensen_shannon_divergence(clique_vecs['clique_' + str(i)], doc_vecs[user])
                     outfile.write('{}\t{}\t{}\n'.format(user, 'clique', jsd))
+
+    distance_dir = output_dir + user_topics_dir + 'community_user_distances/'
+    func = partial(draw_user_to_clique_graphs, distance_dir)
+    pool.map(func, distance_files_to_iter(distance_dir))
+
     pool.close()
 
 if __name__ == '__main__':
