ae3c090385ee81cbd6de4274b4528fef7a55a577
Kenneth Orton <korton@asu.edu>
2016-12-28 05:41:08 -0700

    Update create_LDA_model.py

--- create_LDA_model.py
+++ create_LDA_model.py
@@ -10,7 +10,6 @@ import gensim
 from gensim.corpora import MmCorpus, Dictionary, WikiCorpus
 from gensim import models
 import pyLDAvis
-import pyLDAvis.gensim as gensimvis
 import argparse
 
 DEFAULT_DICT_SIZE = 100000
@@ -20,29 +19,28 @@ ignore_words = set(stopwords.words('english'))
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-def list_to_gen(tweets, directory):
-    for filename in tweets:
-        yield directory + str(filename)
-
 def preprocess_tweet(tweet):
     with open(tweet, 'r') as infile:
         # transform tweet document into one string
         text = ' '.join(line.rstrip('\n') for line in infile)
-        # remove emoji's
-        try:
-            reg_ex = re.compile(u'([\U0001F300-\U0001F64F])|([\U0001F680-\U0001F6FF])|([\U00002600-\U000027BF])')
-        except:
-            reg_ex = re.compile(u'([\u2600-\u27BF])|([\uD83C][\uDF00-\uDFFF])|([\uD83D][\uDC00-\uDE4F])|([\uD83D][\uDE80-\uDEFF])')
-        # remove URLS
-        text = reg_ex.sub('', text)
-        text = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', text)
-        # remove hashtag symbol
-        text = re.sub(r'[^\w]', ' ', text) 
-        text = text.replace("'", "")
-        # remove stopwords and tokenize
-        #return list(gensim.utils.lemmatize(text, allowed_tags=re.compile('(NN)'), stopwords=ignore_words, min_length=3))
-        # remove stopwords and lemmatize
-        return list(gensim.utils.lemmatize(text, stopwords=ignore_words, min_length=3))
+    # remove emoji's
+    try:
+        reg_ex = re.compile(u'([\U0001F300-\U0001F64F])|([\U0001F680-\U0001F6FF])|([\U00002600-\U000027BF])')
+    except:
+        reg_ex = re.compile(u'([\u2600-\u27BF])|([\uD83C][\uDF00-\uDFFF])|([\uD83D][\uDC00-\uDE4F])|([\uD83D][\uDE80-\uDEFF])')
+    # remove URLS
+    text = reg_ex.sub('', text)
+    text = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', text)
+    # remove hashtag symbol
+    text = re.sub(r'[^\w]', ' ', text) 
+    text = text.replace("'", "")
+    # remove stopwords and lemmatize
+    #return list(gensim.utils.lemmatize(text, allowed_tags=re.compile('(NN)'), stopwords=ignore_words, min_length=3))
+    return list(gensim.utils.lemmatize(text, stopwords=ignore_words, min_length=3))
+
+def list_to_gen(directory):
+    for filename in os.listdir(directory):
+        yield directory + str(filename)
 
 class DocumentCorpus(gensim.corpora.TextCorpus):
     def get_texts(self):
@@ -68,16 +66,15 @@ def build_pyLDAvis_output(corp_loc, dict_loc, lda_loc):
     dictionary = Dictionary.load(dict_loc)
     lda = models.LdaModel.load(lda_loc)
     
-    vis_data = gensimvis.prepare(lda, corpus, dictionary)
+    vis_data = pyLDAvis.gensim.prepare(lda, corpus, dictionary)
     pyLDAvis.save_html(vis_data, lda_loc.split('.')[0] + '.html')
 
+# option: text or wiki corpus selector, docs_loc: directory of text docs or location of wiki dump
+# corp_loc: name of output corpus, output_dict: name of output dictionary,
+# num_topics: number of topics for model, output_model: name/location of output model
 
-# python2.7 create_LDA_model.py t dnld_tweets/ tweet_corpus tweet_dict 100 lda_model
+# python2.7 create_LDA_model.py t dnld_tweets/ doc_corpus tweet_dict 100 lda_model
 def main():
-    '''
-    option: text or wiki corpus selector, docs_loc: directory of text docs or location of wiki dump
-    corp_loc: name of output corpus, num_topics: number of topics for model, output_model: name/location of output model
-    '''
     parser = argparse.ArgumentParser(description='Create a corpus from a collection of documents and/or build an LDA model')
     subparsers = parser.add_subparsers(dest='mode')
     
@@ -103,13 +100,13 @@ def main():
     args = parser.parse_args()
 
     if args.mode == 'text':
-        doc_corpus = DocumentCorpus(os.listdir(args.docs_loc))
+        doc_corpus = DocumentCorpus(args.docs_loc)
 
         # ignore words that appear in less than 5 documents or more than 5% of documents
         doc_corpus.dictionary.filter_extremes(no_below=5, no_above=0.5, keep_n=DEFAULT_DICT_SIZE)
 
         MmCorpus.serialize(args.corp_loc + '.mm', doc_corpus)
-        tweet_corpus.dictionary.save(args.corp_loc + '.dict')
+        doc_corpus.dictionary.save(args.corp_loc + '.dict')
 
     if args.mode == 'wiki':
         wiki_corpus = WikiCorpus(args.wiki_loc)
