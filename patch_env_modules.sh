21d61d73870624a5c9b9e972b354df4a6720e9fd
Kenneth Orton <korton@asu.edu>
2017-04-01 04:32:49 -0700

    Add files via upload

---
+++ patch_env_modules.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp patches/wikicorpus.py venv/lib/python2.7/site-packages/gensim/corpora/
+cp patches/prog_class.py venv/lib/python2.7/site-packages/pyprind/
+cp patches/english ~/nltk_data/corpora/stopwords/
