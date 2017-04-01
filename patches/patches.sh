d3c9885bee3ecb7e42f8494b1fe6ec04a53e8432
Kenneth Orton <korton@asu.edu>
2017-04-01 03:26:53 -0700

    Add files via upload

---
+++ patches/patches.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo cp ./wikicorpus.py /usr/local/lib/python2.7/dist-packages/gensim/corpora/wikicorpus.py
+sudo cp ./prog_class.py /usr/local/lib/python2.7/dist-packages/pyprind/prog_class.py
