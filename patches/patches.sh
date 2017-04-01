24ddba2177fd67dc72c0c4586ffa6a9166c6095b
Kenneth Orton <korton@asu.edu>
2017-04-01 03:28:51 -0700

    Update patches.sh

--- patches/patches.sh
+++ patches/patches.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sudo cp ./wikicorpus.py /usr/local/lib/python2.7/dist-packages/gensim/corpora/wikicorpus.py
 sudo cp ./prog_class.py /usr/local/lib/python2.7/dist-packages/pyprind/prog_class.py
+cp ./english ~/nltk_data/corpora/stopwords/
