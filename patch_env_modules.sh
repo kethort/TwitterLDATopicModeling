a585d1f7415497481237c44f3ae4f200f2d4b8b3
Kenneth Orton <korton@asu.edu>
2017-04-01 04:32:32 -0700

    Delete patch_env_modules.sh

--- patch_env_modules.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cp patches/wikicorpus.py venv/lib/python2.7/site-packages/gensim/corpora/
-cp patches/prog_class.py venv/lib/python2.7/site-packages/pyprind/
-cp patches/english ~/nltk_data/corpora/stopwords/
