54cc64d0b4ed70f24825cdfe9438dfa1395f2ac7
Kenneth Orton <korton@asu.edu>
2017-05-23 09:13:09 -0700

    renamed aws helper script

--- manage_aws_s3_bucket.py
+++ manage_aws_s3_bucket.py
@@ -1,19 +1,15 @@
 import boto3
 import sys
 import os
-from csv import DictReader
+import pandas as pd
 import argparse
 import argcomplete
 import logging
 boto3.set_stream_logger('botocore', logging.INFO)
 
-def get_access_creds():
-    with open('../AWS_Keys/credentials.csv', 'r') as csv_file:
-        reader = DictReader(csv_file)
-        for row in reader:
-            aws_access_key = row['Access key ID']
-            aws_secret_key = row['Secret access key']
-    return aws_access_key, aws_secret_key
+def get_access_creds(credentials_csv):
+    df = pd.read_csv(credentials_csv, sep=',')
+    return str(df['Access key ID']), str(df['Secret access key'])
 
 def main():
     parser = argparse.ArgumentParser(description='Upload or download files/directories to or from an S3 bucket')
