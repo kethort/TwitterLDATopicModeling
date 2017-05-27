832b2d1d8e43104b253d13d54cb4ec679077b420
Kenneth Orton <korton@asu.edu>
2017-05-27 09:56:26 -0700

    added option to lemmatize text corpus

--- create_LDA_model.py
+++ create_LDA_model.py
@@ -6,9 +6,10 @@ import re
 import itertools
 import tarfile
 import multiprocessing
+from functools import partial
 import gensim
 from gensim.corpora import MmCorpus, Dictionary, WikiCorpus
-from gensim import models
+from gensim import models, utils
 import pyLDAvis
 from pyLDAvis import gensim as gensim_vis
 import argparse
@@ -22,7 +23,7 @@ ignore_words = set(stopwords.words('english'))
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 
-def preprocess_tweet(document):
+def preprocess_text(lemma, document):
     with open(document, 'r') as infile:
         # transform document into one string
         text = ' '.join(line.rstrip('\n') for line in infile)
@@ -35,6 +36,9 @@ def preprocess_tweet(document):
     # remove symbols excluding the @, # and \s symbol
     text = re.sub(r'[^\w@#\s]', '', text)
     
+    if lemma:
+        return utils.lemmatize(text, stopwords=ignore_words, min_length=3)
+
     # tokenize words using NLTK Twitter Tokenizer
     tknzr = TweetTokenizer()
     text = tknzr.tokenize(text)
@@ -49,10 +53,19 @@ def list_to_gen(directory):
     for filename in os.listdir(directory):
         yield directory + str(filename)
 
-class TweetCorpus(gensim.corpora.TextCorpus):
+class DocCorpus(gensim.corpora.TextCorpus):
+    def __init__(self, docs_loc, lemmatize, dictionary=None, metadata=None):
+        self.docs_loc = docs_loc
+        self.lemmatize = lemmatize
+        self.metadata = metadata
+        if dictionary is None:
+            self.dictionary = Dictionary(self.get_texts())
+        else:
+            self.dictionary = dictionary
     def get_texts(self):
         pool = multiprocessing.Pool(max(1, multiprocessing.cpu_count() - 1))
-        for tokens in pool.imap(preprocess_tweet, list_to_gen(self.input)):
+        func = partial(preprocess_text, self.lemmatize)
+        for tokens in pool.map(func, list_to_gen(self.docs_loc)):
             yield tokens
         pool.terminate()
 
@@ -76,22 +89,19 @@ def build_pyLDAvis_output(corp_loc, dict_loc, lda_loc):
     vis_data = gensim_vis.prepare(lda, corpus, dictionary, sort_topics=False)
     pyLDAvis.save_html(vis_data, lda_loc.split('.')[0] + '.html')
 
-# option: text or wiki corpus selector, docs_loc: directory of text docs or location of wiki dump
-# corp_loc: name of output corpus, output_dict: name of output dictionary,
-# num_topics: number of topics for model, output_model: name/location of output model
-
-# python2.7 create_LDA_model.py t docs_dir/ corpus dictionary 100 lda_model
 def main():
     parser = argparse.ArgumentParser(description='Create a corpus from a collection of tweets and/or build an LDA model')
     subparsers = parser.add_subparsers(dest='mode')
     
-    text_corpus_parser = subparsers.add_parser('tweet', help='Build corpus from directory of tweet files')
+    text_corpus_parser = subparsers.add_parser('text', help='Build corpus from directory of text files')
     text_corpus_parser.add_argument('-d', '--docs_loc', required=True, action='store', dest='docs_loc', help='Directory where tweet documents stored')
     text_corpus_parser.add_argument('-c', '--corp_loc', required=True, action='store', dest='corp_loc', help='Location and name to save corpus')
+    text_corpus_parser.add_argument('-m', '--lemma', action='store_true', dest='lemma', help='Use this option to lemmatize words')
 
     wiki_corpus_parser = subparsers.add_parser('wiki', help='Build corpus from compressed Wikipedia articles')
     wiki_corpus_parser.add_argument('-w', '--wiki_loc', required=True, action='store', dest='wiki_loc', help='Location of compressed Wikipedia dump')
     wiki_corpus_parser.add_argument('-c', '--corp_loc', required=True, action='store', dest='corp_loc', help='Location and name to save corpus')
+    wiki_corpus_parser.add_argument('-m', '--lemma', action='store_true', dest='lemma', help='Use this option to lemmatize words')
 
     lda_model_parser = subparsers.add_parser('lda', help='Create LDA model from saved corpus')
     lda_model_parser.add_argument('-c', '--corp_loc', required=True, action='store', dest='corp_loc', help='Location of corpus')
@@ -108,11 +118,11 @@ def main():
     argcomplete.autocomplete(parser)
     args = parser.parse_args()
 
-    if args.mode == 'tweet':
-        doc_corpus = TweetCorpus(args.docs_loc)
+    if args.mode == 'text':
+        doc_corpus = DocCorpus(args.docs_loc, args.lemma)
 
-        # ignore words that appear in less than 5 documents or more than 50% of documents
-        doc_corpus.dictionary.filter_extremes(no_below=5, no_above=0.5, keep_n=DEFAULT_DICT_SIZE)
+        # ignore words that appear in more than 50% of documents
+        doc_corpus.dictionary.filter_extremes(no_below=1, no_above=0.5, keep_n=DEFAULT_DICT_SIZE)
 
         MmCorpus.serialize(args.corp_loc + '.mm', doc_corpus)
         doc_corpus.dictionary.save(args.corp_loc + '.dict')
