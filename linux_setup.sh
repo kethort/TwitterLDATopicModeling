e9ab4ee515957f44110c226c9a9d766bd2639c7b
acostudent <korton@asu.edu>
2017-06-07 16:52:56 -0700

    added more linux dependencies

--- linux_setup.sh
+++ linux_setup.sh
@@ -6,7 +6,7 @@ sudo apt-get -y upgrade
 
 # install accelerated BLAS environment 
 # openBLAS should be there by default...just in case
-sudo apt-get -y install vim git build-essential libblas-dev liblapack-dev libatlas-base-dev gfortran python-dev libfreetype6-dev libxft-dev awscli
+sudo apt-get -y install vim git build-essential libblas-dev liblapack-dev libatlas-base-dev gfortran python-dev libfreetype6-dev libxft-dev awscli gtk2-engines-pixbuf
 export LAPACK=/usr/lib/liblapack.so
 export ATLAS=/usr/lib/libatlas.so
 
