4fa0014dfdee6ed6a98c46ade6706b3a9b7e4466
Ken Orton <korton@asu.edu>
2017-12-12 22:54:40 -0700

    fixed doc preprocess function

--- tweets_on_LDA.py
+++ tweets_on_LDA.py
@@ -38,8 +38,7 @@ def preprocess_tweet(document, lemma):
     tknzr = TweetTokenizer()
     text = tknzr.tokenize(text)
     # lowercase, remove words less than len 2 & remove numbers in tokenized list
-    text = [word.lower() for word in text if not word.isdigit() and not word in ignore_words]
-    return utils.simple_preprocess(text, deacc=True, min_len=3)
+    return [word.lower() for word in text if not word.isdigit() and not word in ignore_words]
 
 def get_document_vectors(user_id, **kwargs):
     print('Getting document vectors for: ' + user_id)
