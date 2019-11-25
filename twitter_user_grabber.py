79e78d66a19842385ec5c149d7fd503a667a43bd
user <user@windowsxptarget.home>
2019-11-25 02:18:03 -0800

    added cmd line arguments

--- twitter_user_grabber.py
+++ twitter_user_grabber.py
@@ -10,7 +10,10 @@ import requests
 import networkx as nx 
 from networkx.readwrite import json_graph
 from uszipcode import SearchEngine, SimpleZipcode, Zipcode
+import pyprind
 import logging
+import argparse
+import argcomplete
 
 ''' Example script for getting twitter user topology by location '''
 
@@ -33,7 +36,9 @@ def get_user_ids(oauths, latitude, longitude, radius):
 def get_user_followers(oauths, user_ids):
     # returns the followers of each user {user: [followers]}
     user_followers = {}
+    bar = pyprind.ProgPercent(len(set(user_ids)), track_time=True, title='Finding user followers') 
     for user in set(user_ids):
+        bar.update(item_id=str(user) + '\t')
         api = auth.manage_auth_handlers(oauths)
         try: # protected tweets or user doesn't exist
             user_followers[user] = api.followers_ids(id=user)
@@ -42,88 +47,79 @@ def get_user_followers(oauths, user_ids):
 
     return user_followers
 
-def followers_to_users_list(user_followers):
-    user_ids = []
-    for user in user_followers:
-        user_ids += user_followers[user]
-    return set(user_ids)
-
-#def convert_followers_to_edges(followers, user_followers):
-def get_nodes(user_followers):
-    for node in user_followers:
-        yield node
-
-def get_edges(user_followers):
-    for node in user_followers:
-        for edge in user_followers[node]:
-            yield (int(node), edge)
-
 def build_network_graph(graph, nodes, edges):
-    for node in nodes:
-        graph.add_node(int(node))
-    graph.add_edges_from(edges)
+    break
 
 def main():
     search_dir = 'twitter_geo_searches/'
     if not os.path.exists(os.path.dirname(search_dir)):
         os.makedirs(os.path.dirname(search_dir), 0o755)
 
-    search_filename = 'filename.json'
+    oauths = auth.get_access_creds()  
+
+    parser = argparse.ArgumentParser(description='Get twitter user ids and their follower ids from Tweepy')
+    subparsers = parser.add_subparsers(dest='mode')
     
-    oauths = auth.get_access_creds()
-    location_api_key = 'Mx2ltANNNTll9Zk6OJRq4nOYIgDv4GDw9A46YfqKKs6nWmnDSPf1jNISTCGSvAjU'
+    search_parser = subparsers.add_parser('search', help='Gather Twitter user ids and followers by city, state and radius')
+    search_parser.add_argument('-c', '--city', required=True, action='store', dest='city', help='City to search for Twitter user ids')
+    search_parser.add_argument('-s', '--state', required=True, action='store', dest='state', help='State to search for Twitter user ids')   
+    search_parser.add_argument('-r', '--radius', required=True, action='store', dest='radius', help='Radius to search Twitter API for user ids (miles or kilometers -- ex: 50mi or 50km)')   
+    search_parser.add_argument('-f', '--filename', required=True, action='store', dest='filename', help='Name of output file for networkx graph data')   
     
-    city = 'Newport Beach'
-    state = 'CA'
-    search_radius = "50mi" # mi or km
+    netwrkx_parser = subparsers.add_parser('netx', help='Perform operations on already generated networkx graph')
+    netwrkx_parser.add_argument('-q', '--clique', action='store_true', dest='clique', help='Find cliques with networkx')
+    netwrkx_parser.add_argument('-f', '--filename', required=True, action='store', dest='filename', help='Networkx input data filename')   
 
-    zip_search = SearchEngine()
-    zipcodes = zip_search.by_city_and_state(city, state, returns=50)
+    argcomplete.autocomplete(parser)
+    args = parser.parse_args()
 
-    geo_locations = []
-    latitude = longitude = ''
+    if args.mode == 'search':
+        city = search_parser.city
+        state = search_parser.state
+        search_radius = search_parser.radius
+        search_filename = search_parser.filename + '.json'
 
-    # make a tuple packed list of geo-locations from search result  
-    for zipcode in zipcodes:
-        latitude = zipcode.lat
-        longitude = zipcode.lng
-        geo_locations.append((latitude, longitude))
+        zip_search = SearchEngine()
+        zipcodes = zip_search.by_city_and_state(city, state, returns=50)
 
-    user_ids = []
-    user_followers = []
+        user_ids = []
+        user_followers = []
 
-    bar = pyprind.ProgPercent(len(geo_locations), track_time=True, title='Finding user ids') 
-    for location in geo_locations:
-        latitude = location[0]
-        longitude = location[1]
-        print(len(user_ids)) 
-        user_ids.extend(get_user_ids(oauths, latitude, longitude, search_radius))
+        bar = pyprind.ProgPercent(len(zipcodes), track_time=True, title='Finding user ids') 
+        for zipcode in zipcodes:
+            bar.update(item_id=str(zipcode.zipcode) + '\t')
+            latitude = zipcode.lat
+            longitude = zipcode.lng
+            user_ids.extend(get_user_ids(oauths, latitude, longitude, search_radius))
+            
+        user_followers = get_user_followers(oauths, user_ids)
         
-    user_followers = get_user_followers(oauths, user_ids)
-    
-    with open(os.path.join(search_dir, search_filename), 'w') as outfile:
-        json.dump(user_followers, outfile, sort_keys=True, indent=4)
+        with open(os.path.join(search_dir, search_filename), 'w') as outfile:
+            json.dump(user_followers, outfile, sort_keys=True, indent=4)
 
-    filename = os.path.join(search_dir, 'a.json')
-    with open(filename, 'r') as twitter_users:
-        user_followers = json.load(twitter_users)
+        filename = os.path.join(search_dir, 'a.json')
+        with open(filename, 'r') as twitter_users:
+            user_followers = json.load(twitter_users)
 
-    # create networkx graph from dictionary where the nodes are the keys
-    # and the edges are the items in the list(value)
-    graph = nx.Graph()
+        # create networkx graph from dictionary where the nodes are the keys
+        # and the edges are the value lists
+        graph = nx.Graph()
 
-    graph.add_nodes_from(user_followers.keys())
+        graph.add_nodes_from(user_followers.keys())
 
-    for k, v in user_followers.items():
-        graph.add_edges_from(([(k, t) for t in v]))
+        for k, v in user_followers.items():
+            graph.add_edges_from(([(k, t) for t in v]))
 
-    
-    # serialize the graph to disk
-    data = json_graph.node_link_data(graph)
+        
+        # serialize the graph to disk
+        data = json_graph.node_link_data(graph)
 
-    out_file = os.path.join(search_dir, 'graph_data.json')
-    with open(out_file, 'w') as output:
-        json.dump(data, output, sort_keys=True, indent=4)
+        out_file = os.path.join(search_dir, 'graph_data.json')
+        with open(out_file, 'w') as output:
+            json.dump(data, output, sort_keys=True, indent=4)
+
+    if args.mode == 'netx':
+        break
 
 if __name__ == '__main__':
     sys.exit(main())
